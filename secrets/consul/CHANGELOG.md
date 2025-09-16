cdca7d051cc42ca726ef35a0067f1729743f1245
Philipp Stehle <philipp.stehle@secretz.io>
2025-09-16 12:51:06 +0200

    refactor "main.go" to match other plugins
    Signed-off-by: Philipp Stehle <philipp.stehle@secretz.io>

--- secrets/consul/CHANGELOG.md
+++ secrets/consul/CHANGELOG.md
@@ -22,6 +22,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - cleanup docs
 - replace "Vault" with "OpenBao" in docs
 - use OpenBao go packages
+- refactor "main.go" to match other plugins
 
 ### Other
 
--- secrets/consul/cmd/main.go
+++ secrets/consul/cmd/main.go
@@ -4,10 +4,10 @@
 package main
 
 import (
+	"log"
 	"os"
 
-	hclog "github.com/hashicorp/go-hclog"
-	"github.com/openbao/openbao-plugins/secrets/consul"
+	consulsecrets "github.com/openbao/openbao-plugins/secrets/consul"
 	"github.com/openbao/openbao/api/v2"
 	"github.com/openbao/openbao/sdk/v2/plugin"
 )
@@ -20,15 +20,14 @@ func main() {
 	tlsConfig := apiClientMeta.GetTLSConfig()
 	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
 
-	if err := plugin.ServeMultiplex(&plugin.ServeOpts{
-		BackendFactoryFunc: consul.Factory,
+	err := plugin.ServeMultiplex(&plugin.ServeOpts{
+		BackendFactoryFunc: consulsecrets.Factory,
 		// set the TLSProviderFunc so that the plugin maintains backwards
 		// compatibility with Vault versions that don’t support plugin AutoMTLS
 		TLSProviderFunc: tlsProviderFunc,
-	}); err != nil {
-		logger := hclog.New(&hclog.LoggerOptions{})
-
-		logger.Error("plugin shutting down", "error", err)
+	})
+	if err != nil {
+		log.Println(err)
 		os.Exit(1)
 	}
 }
