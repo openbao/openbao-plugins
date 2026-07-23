5fe009e1f2c88d399163246daa13e0ab196c598f
Jonas Köhnen <jonas.koehnen@sap.com>
2026-07-23 13:10:22 +0200

    Switch to new PKCS#11 implementation
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- kms/pkcs11/cmd/main.go
+++ kms/pkcs11/cmd/main.go
@@ -4,15 +4,48 @@
 package main
 
 import (
+	"errors"
+	"flag"
+	"strings"
+
+	"github.com/openbao/go-kms-wrapping/kms/pkcs11/v2"
 	"github.com/openbao/go-kms-wrapping/plugin/v2"
-	"github.com/openbao/go-kms-wrapping/v2"
-	"github.com/openbao/go-kms-wrapping/wrappers/pkcs11/v2"
+	wrapping "github.com/openbao/go-kms-wrapping/v2"
+	"github.com/openbao/go-kms-wrapping/v2/kms"
 )
 
 func main() {
+	aliases := make(map[string]string)
+
+	flag.Func(
+		"alias",
+		"Register a PKCS#11 library path alias. This option is repeatable.",
+		func(s string) error {
+			parts := strings.SplitN(s, "=", 2)
+			if len(parts) != 2 {
+				return errors.New(`aliases are passed as "<name>=<path>"`)
+			}
+			aliases[parts[0]] = parts[1]
+			return nil
+		},
+	)
+
+	flag.Parse()
+
+	opts := []pkcs11.Option{
+		pkcs11.WithAliasesFromEnv(),
+		pkcs11.WithAliases(aliases),
+	}
+
 	plugin.Serve(&plugin.ServeOpts{
+		KMSFactoryFunc: func() kms.KMS {
+			return pkcs11.New(opts...)
+		},
 		WrapperFactoryFunc: func() wrapping.Wrapper {
-			return pkcs11.NewWrapper()
+			return pkcs11.NewWrapper(opts...)
+		},
+		Metadata: plugin.Metadata{
+			SensitiveKMSFields: pkcs11.SensitiveKMSFields,
 		},
 	})
 }
