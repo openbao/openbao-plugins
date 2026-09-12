39086a86c19464d72d2ca7d23d22f398d807f6dd
Luiggi33 <github@luiggi33.de>
2026-09-12 23:03:22 +0200

    fix DN bind getting overwritten by upn
    Signed-off-by: Luiggi33 <github@luiggi33.de>

--- secrets/ldap/client.go
+++ secrets/ldap/client.go
@@ -15,6 +15,7 @@ import (
 
 type ldapClient interface {
 	UpdateDNPassword(conf *client.Config, dn string, newPassword string) error
+	UpdateBindPassword(conf *client.Config, newPassword string) error
 	UpdateUserPassword(conf *client.Config, user, newPassword string) error
 	Execute(conf *client.Config, entries []*ldif.Entry, continueOnError bool) error
 }
@@ -38,28 +39,26 @@ func (c *Client) UpdateDNPassword(conf *client.Config, dn string, newPassword st
 		client.FieldRegistry.ObjectClass: {"*"},
 	}
 
-	userAttr := conf.UserAttr
-	if userAttr == "" {
-		userAttr = defaultUserAttr(conf.Schema)
-	}
-	field := client.FieldRegistry.Parse(userAttr)
-	if field == nil {
-		return fmt.Errorf("unsupported userattr %q", userAttr)
+	return c.updatePassword(conf, dn, scope, filters, newPassword)
+}
+
+// UpdateBindPassword updates the password for the object the secret engine binds with.
+// This object could be defined via the upn format, where the dn is the username.
+func (c *Client) UpdateBindPassword(conf *client.Config, newPassword string) error {
+	dn := conf.BindDN
+	scope := ldap.ScopeBaseObject
+	filters := map[*client.Field][]string{
+		client.FieldRegistry.ObjectClass: {"*"},
 	}
 
-	if field == client.FieldRegistry.UserPrincipalName {
+	if conf.UPNDomain != "" {
 		scope = ldap.ScopeWholeSubtree
 		bindUser := fmt.Sprintf("%s@%s", ldap.EscapeFilter(dn), conf.UPNDomain)
-		filters[field] = []string{bindUser}
+		filters[client.FieldRegistry.UserPrincipalName] = []string{bindUser}
 		dn = conf.UserDN
 	}
 
-	newValues, err := client.GetSchemaFieldRegistry(conf.Schema, newPassword)
-	if err != nil {
-		return fmt.Errorf("error updating password: %s", err)
-	}
-
-	return c.ldap.UpdatePassword(conf, dn, scope, newValues, filters)
+	return c.updatePassword(conf, dn, scope, filters, newPassword)
 }
 
 // UpdateUserPassword updates the password for the object with the given username.
@@ -78,12 +77,16 @@ func (c *Client) UpdateUserPassword(conf *client.Config, username string, newPas
 		field: {username},
 	}
 
+	return c.updatePassword(conf, conf.UserDN, ldap.ScopeWholeSubtree, filters, newPassword)
+}
+
+func (c *Client) updatePassword(conf *client.Config, dn string, scope int, filters map[*client.Field][]string, newPassword string) error {
 	newValues, err := client.GetSchemaFieldRegistry(conf.Schema, newPassword)
 	if err != nil {
 		return fmt.Errorf("error updating password: %s", err)
 	}
 
-	return c.ldap.UpdatePassword(conf, conf.UserDN, ldap.ScopeWholeSubtree, newValues, filters)
+	return c.ldap.UpdatePassword(conf, dn, scope, newValues, filters)
 }
 
 func (c *Client) Execute(conf *client.Config, entries []*ldif.Entry, continueOnError bool) (err error) {
